bbc03642635fb91f1c2ee1f62d2cf6fe0db58343
FilipeJava <phill.java@gmail.com>
2023-10-02 18:33:07 -0300

    ajustes de registro

--- src/app/registro/RegistroRow.jsx
+++ src/app/registro/RegistroRow.jsx
@@ -4,7 +4,7 @@ import { ClipboardDocumentListIcon } from '@heroicons/react/24/solid';
 
 export default function RegistroRow({ registro }) {
     const corTexto = registro.tipo === 'EXERCICIO' ? 'text-green-200' : 'text-red-500';
-
+    //ajuste
     return (
         <div id='data-row' className={`flex justify-between hover:bg-slate-700 p-2 my-2 cursor-pointer rounded ${corTexto}`}>
             <ClipboardDocumentListIcon className="h-6 w-6" />
