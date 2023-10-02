ec6d992a5b3b56d6741d84b5091849d79ca55cc3
FilipeJava <phill.java@gmail.com>
2023-10-02 18:15:14 -0300

    Estilização cor registro

--- src/app/registro/RegistroRow.jsx
+++ src/app/registro/RegistroRow.jsx
@@ -1,15 +1,18 @@
+import React from 'react';
 import BasicMenu from '@/components/BasicMenu';
 import { ClipboardDocumentListIcon } from '@heroicons/react/24/solid';
 
-export default function RegistroRow  ({registro}){
-    return(
-        <div id='data-row' className='flex justify-between hover:bg-slate-700 p-2 my-2 cursor-pointer rounded' >
+export default function RegistroRow({ registro }) {
+    const corTexto = registro.tipo === 'EXERCICIO' ? 'text-green-200' : 'text-red-500';
+
+    return (
+        <div id='data-row' className={`flex justify-between hover:bg-slate-700 p-2 my-2 cursor-pointer rounded ${corTexto}`}>
             <ClipboardDocumentListIcon className="h-6 w-6" />
-              <span>{registro.tipo}</span>
-              <span>{registro.descricao}</span>
-              <span>{registro.data}</span>
-              <span>{registro.calorias}</span>
-              <BasicMenu registro={registro}/>
-            </div>      
+            <span>{registro.tipo}</span>
+            <span>{registro.descricao}</span>
+            <span>{registro.data}</span>
+            <span>{registro.calorias}</span>
+            <BasicMenu registro={registro} />
+        </div>
     );
 }
