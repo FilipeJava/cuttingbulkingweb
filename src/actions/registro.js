ef0ffd8d770756855cb330566f716ce68615b2fa
FilipeJava <phill.java@gmail.com>
2023-10-02 17:35:06 -0300

    ajuste resposta

--- src/actions/registro.js
+++ src/actions/registro.js
@@ -13,7 +13,7 @@ const options = {
     body: JSON.stringify(Object.fromEntries(formData))
 }
     const resp = fetch(url, options)
-    if ( (await resp).status !== 200){
+    if ( (await resp).status !== 201){
         return {message : "Erro ao cadastrar"}
     }
     revalidatePath("/registro")
